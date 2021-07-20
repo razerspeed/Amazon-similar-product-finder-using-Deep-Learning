ed537c5748a7d1cebd49aa5716460323a7b59e79
rohit kumar <45831610+razerspeed@users.noreply.github.com>
2021-07-20 13:37:22 +0530

    Add files via upload

---
+++ model_save.py
@@ -0,0 +1,283 @@
+
+# Preliminaries
+from tqdm import tqdm
+import math
+
+import pandas as pd
+import numpy as np
+
+# Visuals and CV2
+import cv2
+
+# albumentations for augs
+import albumentations
+from albumentations.pytorch.transforms import ToTensorV2
+
+#torch
+import torch
+import timm
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torch.utils.data import Dataset,DataLoader
+
+import gc
+
+from sklearn.neighbors import NearestNeighbors
+
+
+
+DIM = (512,512)
+
+BATCH_SIZE = 12
+SEED = 2020
+device = torch.device('cpu')
+CLASSES = 11014
+CHECK_SUB = False
+GET_CV = False
+model_name = 'efficientnet_b3'
+# MODEL PATH
+IMG_MODEL_PATH = 'model_efficientnet_b3_IMG_SIZE_512_arcface.bin'
+#Metric Loss and its params
+loss_module = 'arcface' #'cosface' #'adacos'
+s = 30.0
+m = 0.5 
+ls_eps = 0.0
+easy_margin = False
+
+
+# # Loading Data
+
+def read_dataset():
+
+    df = pd.read_csv('test1.csv')
+    image_paths = 'test_images/' + df['image']
+        
+    return df, image_paths
+
+def get_neighbors(df, embeddings, KNN = 10, image = True):
+    
+    model = NearestNeighbors(n_neighbors = KNN)
+    model.fit(embeddings)
+    distances, indices = model.kneighbors(embeddings)
+    
+
+    predictions = []
+    for k in tqdm(range(embeddings.shape[0])):
+        if image:
+            idx = np.where(distances[k,] < 2.7)[0]
+            
+        ids = indices[k,idx]
+        posting_ids = df['posting_id'].iloc[ids].values
+        predictions.append(posting_ids)
+        
+    del model, distances, indices
+    gc.collect()
+    return df, predictions
+
+def get_test_transforms():
+
+    return albumentations.Compose(
+        [
+            albumentations.Resize(DIM[0],DIM[1],always_apply=True),
+            albumentations.Normalize(),
+        ToTensorV2(p=1.0)
+        ]
+    )
+
+
+class ShopeeDataset(Dataset):
+    def __init__(self, image_paths, transforms=None):
+
+        self.image_paths = image_paths
+        self.augmentations = transforms
+
+    def __len__(self):
+        return self.image_paths.shape[0]
+
+    def __getitem__(self, index):
+        image_path = self.image_paths[index]
+        
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        
+        if self.augmentations:
+            augmented = self.augmentations(image=image)
+            image = augmented['image']       
+        
+        
+        return image,torch.tensor(1)
+
+
+
+class ShopeeNet(nn.Module):
+
+    def __init__(self,
+                 n_classes,
+                 model_name='efficientnet_b0',
+                 use_fc=False,
+                 fc_dim=512,
+                 dropout=0.0,
+                 loss_module='softmax',
+                 s=30.0,
+                 margin=0.50,
+                 ls_eps=0.0,
+                 theta_zero=0.785,
+                 pretrained=False):
+        """
+        :param n_classes:
+        :param model_name: name of model from pretrainedmodels
+            e.g. resnet50, resnext101_32x4d, pnasnet5large
+        :param pooling: One of ('SPoC', 'MAC', 'RMAC', 'GeM', 'Rpool', 'Flatten', 'CompactBilinearPooling')
+        :param loss_module: One of ('arcface', 'cosface', 'softmax')
+        """
+        super(ShopeeNet, self).__init__()
+        print('Model building for {} backbone'.format(model_name))
+
+        self.backbone = timm.create_model(model_name, pretrained=pretrained)
+        final_in_features = self.backbone.classifier.in_features
+        
+        self.backbone.classifier = nn.Identity()
+        self.backbone.global_pool = nn.Identity()
+        
+        self.pooling =  nn.AdaptiveAvgPool2d(1)
+            
+        self.use_fc = use_fc
+        if use_fc:
+            self.dropout = nn.Dropout(p=dropout)
+            self.fc = nn.Linear(final_in_features, fc_dim)
+            self.bn = nn.BatchNorm1d(fc_dim)
+            self._init_params()
+            final_in_features = fc_dim
+
+        self.loss_module = loss_module
+        if loss_module == 'arcface':
+            self.final = ArcMarginProduct(final_in_features, n_classes,
+                                          s=s, m=margin, easy_margin=False, ls_eps=ls_eps)
+        else:
+            self.final = nn.Linear(final_in_features, n_classes)
+
+    def _init_params(self):
+        nn.init.xavier_normal_(self.fc.weight)
+        nn.init.constant_(self.fc.bias, 0)
+        nn.init.constant_(self.bn.weight, 1)
+        nn.init.constant_(self.bn.bias, 0)
+
+    def forward(self, x, label):
+        feature = self.extract_feat(x)
+        if self.loss_module in ('arcface', 'cosface', 'adacos'):
+            logits = self.final(feature, label)
+        else:
+            logits = self.final(feature)
+        return feature,logits
+
+    def extract_feat(self, x):
+        batch_size = x.shape[0]
+        x = self.backbone(x)
+        x = self.pooling(x).view(batch_size, -1)
+
+        if self.use_fc:
+            x = self.dropout(x)
+            x = self.fc(x)
+            x = self.bn(x)
+
+        return x
+
+class ArcMarginProduct(nn.Module):
+    r"""Implement of large margin arc distance: :
+        Args:
+            in_features: size of each input sample
+            out_features: size of each output sample
+            s: norm of input feature
+            m: margin
+            cos(theta + m)
+        """
+    def __init__(self, in_features, out_features, s=30.0, m=0.50, easy_margin=False, ls_eps=0.0):
+        super(ArcMarginProduct, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.s = s
+        self.m = m
+        self.ls_eps = ls_eps  # label smoothing
+        self.weight = Parameter(torch.FloatTensor(out_features, in_features))
+        nn.init.xavier_uniform_(self.weight)
+
+        self.easy_margin = easy_margin
+        self.cos_m = math.cos(m)
+        self.sin_m = math.sin(m)
+        self.th = math.cos(math.pi - m)
+        self.mm = math.sin(math.pi - m) * m
+
+    def forward(self, input, label):
+        # --------------------------- cos(theta) & phi(theta) ---------------------------
+        cosine = F.linear(F.normalize(input), F.normalize(self.weight))
+        sine = torch.sqrt(1.0 - torch.pow(cosine, 2))
+        phi = cosine * self.cos_m - sine * self.sin_m
+        if self.easy_margin:
+            phi = torch.where(cosine > 0, phi, cosine)
+        else:
+            phi = torch.where(cosine > self.th, phi, cosine - self.mm)
+        # --------------------------- convert label to one-hot ---------------------------
+        # one_hot = torch.zeros(cosine.size(), requires_grad=True, device='cuda')
+        one_hot = torch.zeros(cosine.size(), device='cuda')
+        one_hot.scatter_(1, label.view(-1, 1).long(), 1)
+        if self.ls_eps > 0:
+            one_hot = (1 - self.ls_eps) * one_hot + self.ls_eps / self.out_features
+        # -------------torch.where(out_i = {x_i if condition_i else y_i) -------------
+        output = (one_hot * phi) + ((1.0 - one_hot) * cosine)
+        output *= self.s
+
+        return output
+
+
+
+
+def get_image_embeddings(image_paths):
+    embeds = []
+    
+    model = ShopeeNet(n_classes=CLASSES,model_name=model_name)
+    model.eval()
+
+    model.load_state_dict(torch.load(IMG_MODEL_PATH,map_location=torch.device('cpu')),strict=False)
+    model = model.to(device)
+
+    image_dataset = ShopeeDataset(image_paths=image_paths,transforms=get_test_transforms())
+    image_loader = torch.utils.data.DataLoader(
+        image_dataset,
+        batch_size=BATCH_SIZE,
+        pin_memory=True,
+        drop_last=False
+    )
+    
+    
+    with torch.no_grad():
+        for img,label in tqdm(image_loader): 
+            img = img.to(device)
+            label = label.to(device)
+            feat, _ = model(img,label)
+            image_embeddings = feat.detach().cpu().numpy()
+            embeds.append(image_embeddings)
+    
+    
+    del model
+    image_embeddings = np.concatenate(embeds)
+    print(f'Our image embeddings shape is {image_embeddings.shape}')
+    del embeds
+    gc.collect()
+    return image_embeddings
+
+
+def predict():
+    df,image_paths = read_dataset()
+
+    image_embeddings = get_image_embeddings(image_paths.values)
+
+    df,image_predictions = get_neighbors(df, image_embeddings, KNN = 10, image = True)
+
+    df['image_predictions'] = image_predictions
+
+    df.to_pickle("prediction.pkl")
+
+
+
+
