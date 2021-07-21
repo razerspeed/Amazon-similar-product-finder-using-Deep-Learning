9e610a328df54dab789a78b862fef1718a5b5b68
rohit kumar <45831610+razerspeed@users.noreply.github.com>
2021-07-21 06:32:44 +0530

    Update download.py

--- download.py
+++ download.py
@@ -4,6 +4,6 @@ import gdown
 def download_from_drive():
     print("downloading model")
     url = 'https://drive.google.com/uc?id=1Xc9te-Q8teJHrE6omfa0YNxP0l5TgBmm'
-    output = 'model_efficientnet_b3_IMG_SIZE_512_arcface.bin'
+    output = 'model_efficientnet_b0_IMG_SIZE_256_arcface.bin'
     gdown.download(url, output, quiet=False)
 
