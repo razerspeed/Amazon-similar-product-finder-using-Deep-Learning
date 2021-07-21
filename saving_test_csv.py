068407b51c53f03e3cf3e41b7f373fe4194a6bea
rohit kumar <45831610+razerspeed@users.noreply.github.com>
2021-07-21 07:32:17 +0530

    Update saving_test_csv.py

--- saving_test_csv.py
+++ saving_test_csv.py
@@ -15,8 +15,7 @@ nltk.download('words')
 
 
 transtable = str.maketrans('', '', string.punctuation)
-HEADERS = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate",
-           "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/65.0'}
 
 
 def delete_files():
